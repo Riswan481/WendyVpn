ee4d68bb989b0b6cb0cc5d55ce73fcd6c766ef9c
JESSTUNNEL <143738746+Riswan481@users.noreply.github.com>
2025-03-19 13:56:28 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt install jq curl -y
-domain=ssh-prem.xyz
+domain=rasiya.cloud
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 IP=$(wget -qO- icanhazip.com)
 CF_KEY="1781894fb664d5af0011b8fb79234a9a151ed"
